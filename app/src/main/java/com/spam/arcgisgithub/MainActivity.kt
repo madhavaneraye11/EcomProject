65cc7938b93110c44f241f3aac446ec3c36b5ec4
madhavaneraye11@gmail.com <MyLifeDadMom@143>
2023-01-05 15:14:54 +0530

    User Store 3 - Commit 4

--- app/src/main/java/com/spam/arcgisgithub/MainActivity.kt
+++ app/src/main/java/com/spam/arcgisgithub/MainActivity.kt
@@ -2,12 +2,18 @@ package com.spam.arcgisgithub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //some thing
+        val a=3
+        val b=2
+        var c=a+b
+        Log.d("Cal","Cal")
 
     }
 }
